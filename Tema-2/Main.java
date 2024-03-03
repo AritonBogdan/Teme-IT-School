de226f42ef580f7fb9835f3872245beeac0ced79
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-03 14:51:04 +0200

    am modificat declararea variabilelor de tip int pe o singura linie

--- Tema-2/Main.java
+++ Tema-2/Main.java
@@ -1,13 +1,11 @@
 public class Main {
-//    Tema-2:
+    //    Tema-2:
 //            -crearea unei aplicații Java.
 //            -crearea a patru variabile cu  valorile de 3, 4, 100, "Nume"
 //            -afișarea acestor variabile în consola din IntelliJ
-    public static void main(String[] args){
-        int a=3;
-        int b=4;
-        int c=100;
-        String d="Nume";
+    public static void main(String[] args) {
+        int a = 3, b = 4, c = 100;
+        String d = "Nume";
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
