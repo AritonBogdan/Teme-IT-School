3cb1f51c54e5e487761093db8ea76a94bb5dc87f
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-03 14:57:33 +0200

    Am facut afisare/printare variabile intr-o singura afirmatie/linie de cod

--- Tema-2/Main.java
+++ Tema-2/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static void main(String[] args) {
         int a = 3, b = 4, c = 100;
         String d = "Nume";
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//        System.out.println(d);
+        System.out.printf("a=%d,b=%d,c=%d,d=%s",a,b,c,d);
     }
 }
