50dece11ee3c73f654fbbdf0cec845be950e657d
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-03 15:19:33 +0200

    Am adaugat raspunsurile la intrebari

--- Tema-3/Tema_3.java
+++ Tema-3/Tema_3.java
@@ -1,5 +1,14 @@
 public class Tema_3 {
     public static void main(String[] args){
-//        Tema 3 in curs de procesare
+//        Tema-3:
+//-crearea unui nou repository în github
+//-copierea proiectului local
+//-crearea unui nou branch
+//-care este denumirea branch-ului principal?
+        /* Branch2 */
+//-care este comanda prin care putem sa ne reîntoarcem pe branch-ul main?
+        /* cd */
+//
+//Intrebarile de teorie le vom adaugă într-un fișier separat
     }
 }
