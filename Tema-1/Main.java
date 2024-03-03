7587cd3b43247fe893a48823475ebff4366e63fb
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-03 10:23:41 +0200

    Am incarcat tema 2

---
+++ Tema-1/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        int a=3;
+        int b=4;
+        int c=100;
+        String d="Nume";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
