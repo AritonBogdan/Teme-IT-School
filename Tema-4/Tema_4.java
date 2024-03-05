f873f93c838bb8006cc3a6f51876bee3c3d6f703
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-05 07:32:44 +0200

    am dat 3 exemple de comenzi git si le-am explicat

--- Tema-4/Tema_4.java
+++ Tema-4/Tema_4.java
@@ -1,7 +1,10 @@
 public class Tema_4 {
-    public static void main(String[] args){
-        int a=5;
-        String b="Tema 4";
-        System.out.printf("a=%d,b=%s",a,b);
+    public static void main(String[] args) {
+        int a = 5;
+        String b = "Tema 4";
+        System.out.printf("a=%d,b=%s", a, b);
+//        git add numefisier -	Pentru a pune fisierele in modul de stage (adica sa fie urmarite ce modificari i se aduc)
+//        git log - vom verifica ce commit-uri s-au facut!
+//        git branch -	Cream un alt branch
     }
 }
