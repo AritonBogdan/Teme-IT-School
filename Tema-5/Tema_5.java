34b02d7c5124107c3307e06c0ee83d6eaacf1a29
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-05 07:50:45 +0200

    am adaugat enuntul primei cerinte din challenge curs 03

--- Tema-5/Tema_5.java
+++ Tema-5/Tema_5.java
@@ -1,5 +1,6 @@
 public class Tema_5 {
     public static void main(String[] args){
+//  1. Write a Java program to take two numbers as input and display the result of addition, subtraction, multiplication, division, and modulus.
         int a=4, b=7;
         System.out.println(a+b);
         System.out.println(a-b);
