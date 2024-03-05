ca8b651fde4073abb7c98e8a97aa42adbd52b970
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-05 07:48:38 +0200

    Am adaugat 1 cerinta sin challenge curs 03

---
+++ Tema-5/Tema_5.java
@@ -0,0 +1,9 @@
+public class Tema_5 {
+    public static void main(String[] args){
+        int a=4, b=7;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
