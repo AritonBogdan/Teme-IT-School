97fe5a564f1e7244f9a98da55f8e40564c4bf67e
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-05 07:38:23 +0200

    am adaugat enuntul problemei si in brnachul master

--- Tema-4/Tema_4.java
+++ Tema-4/Tema_4.java
@@ -1,5 +1,13 @@
 public class Tema_4 {
     public static void main(String[] args){
+//        Tema-4
+//                -crearea unui nou branch.
+//                -upload a 3 fișiere in repository-ul respectiv.
+//                -crearea unui pull request.
+//                -review+acceptarea pull request-ului anterior.
+//                -dați exemple de 3 comenzi de git+explicați ce rol au.
+//
+//                -bonus: din slide curs 02 rezolvam cel puțin primele 2 exerciții din rubrica Challenge.
         int a=5;
         String b="Tema 4";
         System.out.printf("a=%d,b=%s",a,b);
