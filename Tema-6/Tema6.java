2e84bb65fbaffd92aabe7ba49dceed71103b9bfb
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-18 00:24:54 +0200

    Challenge-urile din cursul 03 se regasesc toate in Tema 5 iar intrebarile din slide-urile 27 curs 03 le-am facut aici in Tema 6

---
+++ Tema-6/Tema6.java
@@ -0,0 +1,56 @@
+public class Tema6 {
+    public static void main(String[] args) {
+        /** Challenge-urile din cursul 03 se regasesc in Tema-5, le-am facut acolo pe toate */
+
+        /**Slide-ul 27 - Curs 03 */
+
+//        What is casting Java?
+        /** In Java, casting refers to the process of converting a value from one data type to another.
+         * This is necessary when you want to assign a value of one data type to a variable of another
+         * data type that is not directly compatible with it. There are two types of casting in Java:
+         * Implicit Casting (Widening Casting): Implicit casting occurs when you assign a value of a
+         * smaller data type to a variable of a larger data type. Java automatically performs this conversion
+         * because there is no risk of data loss.
+         * For example:
+         * int numInt = 100;
+         * double numDouble = numInt; // Implicit casting from int to double
+         * Explicit Casting (Narrowing Casting): Explicit casting occurs when you assign a value of a larger
+         * data type to a variable of a smaller data type. In this case, you need to explicitly specify the type
+         * conversion by placing the desired type in parentheses before the value to be cast. This type of casting
+         * might result in data loss or truncation. For example:
+         * double numDouble = 100.55;
+         * int numInt = (int) numDouble; // Explicit casting from double to int */
+
+//        What is memory reference?
+        /** A memory reference, also known as a memory address or pointer, is a value that indicates the location
+         * of a specific piece of data in computer memory. In simple terms, it's like a unique identifier or "address"
+         * that allows programs to access and manipulate data stored in memory.*/
+
+//        What are decision constructs in Java? if, else?
+        /** In Java, decision constructs are programming structures that allow you to make decisions based
+         * on certain conditions. The most common decision constructs in Java are the if, else if, and else statements.
+         * if statement: The if statement allows you to execute a block of code only if a specified condition is true.
+         * If the condition evaluates to true, the code inside the if block is executed. If the condition
+         * evaluates to false, the code inside the if block is skipped. The syntax of the if statement is as follows
+         * if (condition) {// Code to be executed if the condition is true
+         * }
+         * else statement: The else statement allows you to specify a block of code to be executed if none of
+         * the preceding conditions in the if and else if statements are true.
+         * It is optional and follows an if or else if statement. The syntax of the else statement is as follows:
+         * if (condition) {
+         *     // Code to be executed if the condition is true
+         * } else {
+         *     // Code to be executed if the condition is false
+         * }
+         * else if statement: The else if statement allows you to specify additional conditions to be tested if the
+         * first if condition is false. It follows an if statement and its condition is evaluated
+         * only if the preceding if condition(s) evaluate to false. The syntax of the else if statement is as follows:
+         * if (condition1) {
+         *     // Code to be executed if condition1 is true
+         * } else if (condition2) {
+         *     // Code to be executed if condition1 is false and condition2 is true
+         * } else {
+         *     // Code to be executed if both condition1 and condition2 are false
+         * }*/
+    }
+}
