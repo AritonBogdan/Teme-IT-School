405ecac7dff02f9e3582ff360829823738149b91
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-19 16:01:39 +0200

    Am incarcat Tema8

---
+++ Tema-8/Tema8.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Tema8 {
+    public static void main(String[] args) {
+/**1.Create a simple program that takes a number as input and uses if-else statements to
+ * categorize the number as "positive," "negative," or "zero." */
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Insert a number");
+//        int number = scanner.nextInt();
+//        if (number > 0) {
+//            System.out.println("The number " + number + " is positive");
+//        } else if (number < 0) {
+//            System.out.println("The number " + number + " is negative");
+//        } else{
+//            System.out.println("The number is equal to zero");
+//        }
+
+        /** 2.Create a program that takes a number as input and uses if statement to print whether the number
+         * is "positive" or "non-positive" */
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Insert the number");
+//        int number2 = scanner.nextInt();
+//        if (number2 > 0){
+//            System.out.println("The number is positive");
+//        }else {
+//            System.out.println("The number is non-positive");
+//        }
+
+/**3.Create a program that takes three numbers as input and print the largest number among them. */
+//        int x = 2, y = 3, a = 6;
+//        int ln = x;
+//        if (y > ln) {
+//            ln = (int) y;
+//        }
+//        if (a > ln) {
+//            ln = (int) a;
+//        }
+//        System.out.println("The largest number is " + ln);
+
+/**Challenge 17.Curs 04 -  Write a Java program that simulates a simple authentication system. Declare a boolean variable hasUsername
+ * and hasPassword. Set up a series of logical conditions using these two variables that will check the following conditions
+ * - if both has Username and hasPassword are true, print "Authentication successful"
+ * - if either hasUsername or hasPassword is false print "Authentication failed"
+ * - if hasUsername is true but hasPassword is false print Password incorrect*/
+
+        /**Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert the boolean values:");
+        int firstB = scanner.nextInt();
+        int secondB = scanner.nextInt();*/
+//        boolean hasUsername = true;
+//        boolean hasPassword = true;
+//        if (hasUsername && hasPassword) {
+//            System.out.println("Authentication successful");
+//        } else if (hasUsername || hasPassword){
+//            System.out.println("Authentication failed");
+//        }else if (hasUsername && !(hasPassword)){
+//            System.out.println("Password incorect");
+//        }else{
+//            System.out.println("Username incorect");
+//        }
+    }
+}
