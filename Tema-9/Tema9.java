2605ee777e0eee568527daa6facd4e7af3998111
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-19 16:49:28 +0200

    Am adaugat Tema9

---
+++ Tema-9/Tema9.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Tema9 {
+    public static void main(String[] args) {
+/** Create a program that defines a string variable with a value of "Hello".
+ *  Use nested if statements to first check if the length of the string is greater than 5.
+ *  If it is not, check if the string equals "Hello" and print the appropriate message in each case.*/
+//        String variabila = "Hello";
+//        if (variabila.length() > 5) {
+//            System.out.println("Variabila are mai mult de 5 litere");
+//        } else if (variabila.equals("Hello")) {
+//            System.out.println("Variabila are 5 litere");
+//        }
+
+        /** Create a Java program where you define a char variable with a value of 'A'. Use a switch statement to print
+         *  a message for the values 'A', 'B', and 'C'. For 'A', print "Excellent"; for 'B', print "Good";
+         *  for 'C', print "Average"*/
+
+        /**Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce a letter:");
+        String variabila = scanner.nextLine();
+        char var;
+        if (variabila.length() > 0) {
+            var = variabila.charAt(0);
+        } else {
+            System.out.println("Invalid input. Please enter a valid grade character.");
+            return;
+        }
+        char var = variabila;*/
+//        char var = 'A';  //"variabila";
+//        switch (var) {
+//            case 'A':
+//                System.out.println("Excellent");
+//                break;
+//            case 'B':
+//                System.out.println("Good");
+//                break;
+//            case 'C':
+//                System.out.println("Average");
+//                break;
+//        }
+
+/**Create a program that defines a string variable with a value of "Winter". Use a switch statement to print
+ * a message for each season ("Winter", "Spring", "Summer", "Autumn"). For "Winter", print "It's cold";
+ * for "Spring", print "Flowers bloom"; for "Summer", print "It's hot"; for "Autumn", print "Leaves fall" */
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Introduceti un sezon: ");
+//        String var = scanner.nextLine();
+//        String variabilaW = var;
+//        switch (variabilaW){
+//            case "Winter":
+//                System.out.println("It's cold");
+//                break;
+//            case "Spring":
+//                System.out.println("Flowers bloom");
+//                break;
+//            case "Summer":
+//                System.out.println("It's hot");
+//                break;
+//            case "Autumn":
+//                System.out.println("Leaves fall");
+//                break;
+//        }
+    }
+}
