40f5d2aff72b3ea22b94bc9438c0701e3ed67ce8
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-19 15:11:54 +0200

    Am adaugat exercitiile de la Tema 7 (1+2+3)

---
+++ Tema-7/tema7.java
@@ -0,0 +1,38 @@
+public class tema7 {
+    public static void main(String[] args) {
+        /**Tema 7.1 -Create a Java program where you define an integer variable with a value of 5.
+         * Use an if statement to check if the number is greater than 10.If it is, print
+         * "The number is greater than 10";otherwise, print "The number is not greater than 10" */
+//        int number5 = 5;
+//        if (number5 > 10) {
+//            System.out.println("The number " + number5 + " is greater than 10");
+//        } else {
+//            System.out.println("The number " + number5 + " is not greater than 10");
+//        }
+
+        /**Tema 7.2 Create a program that defines a string variable with a value of "Hello".
+         *  Use nested if statements to first check if the length of the string is greater than 5.
+         *  If it is not, check if the string equals "Hello" and print the appropriate message in each case. */
+//        String variabila = "Hello";
+//        if (variabila.length() > 5) {
+//            System.out.println("The word " + variabila + " has more than five letters");
+//        } else if (variabila.equals("Hello")) {
+//            System.out.println("The word " + variabila + " has five letters");
+//        }
+
+
+        /**Tema 7.3 Create a Java program where you define an integer variable with a value of 75.
+         * Use an if-else-if ladder to check the range in which the number falls. Use the ranges 0-50, 51-100,
+         * and 101-150, and print a message for each range specifying which range the number falls into.*/
+//        int variabila2 = 75;
+//        if (variabila2 >= 0 && variabila2 <= 50) {
+//            System.out.println("The number " + variabila2 + " is in the range of 0-50");
+//        } else if (variabila2 >= 51 && variabila2 <= 100){
+//            System.out.println("The number " + variabila2 + " is in the range of 51-100");
+//        } else if (variabila2 >= 101 && variabila2 <= 150){
+//            System.out.println("The number " + variabila2 + " is in the range of 101-150");
+//        }else{
+//            System.out.println("The number is not in the range 0-150");
+//        }
+//    }
+//}
