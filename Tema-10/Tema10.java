5adcd3bf8e1754842cd0896df3d0bb293a48ec6e
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-19 17:37:46 +0200

    Am adaugat exercitiile de la Tema 10 iar fisierul txt il incarc intr-un fisier text

---
+++ Tema-10/Tema10.java
@@ -0,0 +1,65 @@
+public class Tema10 {
+    public static void main(String[] args) {
+/** parcurgem un for care are indexul de la 1 la 1000. Daca indexul=10 afisam un mesaj, daca indexul=100 afisam
+ * un al doilea mesaj, daca  indexul este 900 afisam un al treilea mesaj.*/
+
+//        int i;
+//        for (i = 1; i <= 1000; i++) {
+//            if (i == 10) {
+//                System.out.println("Indexul este: " + i);
+//            } else if (i == 100) {
+//                System.out.println("Indexul este: " + i);
+//            } else if (i == 900) {
+//                System.out.println("Indexul este: " + i);
+//            }
+//        }
+
+
+/** rezolvam exercitiul de mai sus cu while si do while in loc de for */
+
+        /**WHILE*/
+//        int i = 0;
+//        while (i < 1001) {
+//            if (i == 10) {
+//                System.out.println("Indexul este: " + i);
+//            }
+//            if (i == 100) {
+//                System.out.println("Indexul este: " + i);
+//            }
+//            if (i == 900) {
+//                System.out.println("Indexul este: " + i);
+//            }
+//            i++;
+//        }
+
+        /**DO-WHILE*/
+//        int i = 0;
+//        do {
+//            if (i == 10) {
+//                System.out.println("Indexul este: " + i);
+//            }
+//            if (i == 100) {
+//                System.out.println("Indexul este: " + i);
+//            }
+//            if (i == 900) {
+//                System.out.println("Indexul este: " + i);
+//            }
+//            i++;
+//        }while (i < 1001);
+
+/** folosind un switch verificam daca variabila primita de la utilizator este mai mare decat 100.
+ *  Daca este adevarat afisam un mesaj.*/
+
+//        int var = 63;
+//        switch (var > 100 ? 1 : 0) {
+//            case 1:
+//                System.out.println("Valoarea " + var + " este mai mare decât 100");
+//                break;
+//            default:
+//                System.out.println("Valoarea " + var + " este mai mică decat 100");
+//        }
+
+    }
+
+}
+
