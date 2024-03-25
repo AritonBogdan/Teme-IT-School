ac559bc5ffc9a29ac9d196354d4bbc6811e308e3
AritonBogdan <aritonbogdanclaudiu@gmail.com>
2024-03-25 22:11:16 +0200

    Am adaugat tema 11

---
+++ Tema-11/Tema11.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Tema11 {
+    public static void main(String[] args) {
+/**1. Create a Java program where you define an integer variable with a value between 1 and 7. Use a switch statement to print
+ *  the name of the corresponding day of the week. For example, if the variable has a value of 1,
+ *  it should print "Monday"*/
+//        int ziuaSapt = 1;
+//        switch (ziuaSapt) {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//        }
+        /**2. Create a Java program that uses a while loop to find the sum of numbers from 1 to n, where n
+         * is a positive integer entered by the user.*/
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Introduceti un numar: ");
+//        int numar = scanner.nextInt();
+//        if (numar <= 0) {
+//            System.out.println("Invalid input.");
+//            return;
+//        }
+//        int i = 1;
+//        int sum = 0;
+//        while (i <= numar) {
+//            sum += i;
+//            ++i;
+//        }
+//        System.out.println("The sum of numbers from 1 to " + numar + " is: " + sum);
+
+/**3. Create a Java program that uses a do-while loop to print numbers from 10 to 1 in descending order */
+//        int i = 11;
+//        do {
+//            System.out.println(--i);
+//            System.out.println(--i);
+//            System.out.println(--i);
+//            System.out.println(--i);
+//            System.out.println(--i);
+//            System.out.println(--i);
+//            System.out.println(--i);
+//            System.out.println(--i);
+//            System.out.println(--i);
+//            System.out.println(--i);
+//        } while ( i == 0);
+
+/**4. Create a Java program that uses a for loop to print even numbers from 2 to 20 */
+//        int numarPAr = 20;
+//        for (int i = 0; i <= numarPAr; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println(i);
+//            }
+//        }
+    }
+}
